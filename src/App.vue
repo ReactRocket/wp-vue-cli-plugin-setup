23ef87ac3becf6daeeabdca466d6b590ea38dbb0
AYUSH VARMA <153015142+ReactRocket@users.noreply.github.com>
2024-06-28 23:03:36 +0530

    Added

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,14 @@
     <h1>Vue Setup Completed!</h1>
     <input type="text" v-model="message" placeholder="Type here...">
     <p>Message from Vue: {{ message }}</p>
+    <test-comp/>
   </div>
 </template>
 
 <script>
+import TestComp from './components/TestComp.vue';
 export default {
+  components: { TestComp },
   data() {
     return {
       message: "Hello, Vue!"
---
+++ src/components/TestComp.vue
@@ -0,0 +1,13 @@
+<template >
+    <div>
+        <h1>I am test component</h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
